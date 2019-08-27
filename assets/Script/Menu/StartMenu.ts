12337d954d08b0ec0faebe92e29b5744875c73a4
shumurai <shu.murai3@gmail.com>
2019-08-27 21:27:21 +0900

    resoures追加

--- assets/Script/Menu/StartMenu.ts
+++ assets/Script/Menu/StartMenu.ts
@@ -7,23 +7,22 @@ export default class StartMenu extends cc.Component {
     @property(cc.Node)
     content: cc.Node = null;
 
-    // Item:button -> MusicTitle:label
-    @property(cc.Prefab)
     itemPrefab: cc.Prefab = null;
-
     musics: Item[] = new Array();
 
     // LIFE-CYCLE CALLBACKS:
 
-    // onLoad () {}
+    onLoad() {
+        cc.loader.loadRes('prefab/Item', cc.Prefab, (err, item) => {
+            this.itemPrefab = item;
+        });
+    }
 
     start() {
         // TODO: Get Resources From Network
         this.forDebug();
 
         let array = this.musics;
-        let maxWidth: number = this.content.width;
-        let totalWidth: number = 0;
         // 7以下の場合（あり得ない）は足りるように追加（後ほどあり得るかもしれないけど…）
         // while (array.length < 7) {
         //     let add = array[array.length - 2]
@@ -33,10 +32,10 @@ export default class StartMenu extends cc.Component {
         array.forEach((musicinfo, idx) => {
             let item = cc.instantiate(this.itemPrefab);
             item.children[1].getComponent(cc.Label).string = musicinfo.title;
+            let audioSource = item.getComponent(cc.AudioSource);
             item.parent = this.content;
             let audioID: number;
-            cc.loader.loadResDir(cc.url.raw('resources/musics/ADDrumnBass3'), cc.AudioClip, (err, clip: cc.AudioClip[]) => {
-                cc.log(cc.url.raw('resources/musics/ADDrumnBass3'));
+            cc.loader.loadResDir(musicinfo.soundPath, cc.AudioClip, (err, clip: cc.AudioClip[]) => {
                 if (err) {
                     cc.error(err);
                     return;
@@ -44,31 +43,21 @@ export default class StartMenu extends cc.Component {
                 if (cc.audioEngine.isMusicPlaying) {
                     cc.audioEngine.stopAll();
                 }
-                cc.audioEngine.playMusic(clip[idx], false)
+                audioSource.clip = clip[idx];
             });
-            // cc.loader.loadRes(musicinfo.soundPath, cc.AudioClip, (err, clip: cc.AudioClip) => {
-            //     if (err) {
-            //         cc.error(err.message || err);
-            //         return;
-            //     }
-            //     if (cc.audioEngine.isMusicPlaying) {
-            //         cc.audioEngine.stopAll();
-            //     }
-            //     cc.audioEngine.playMusic(clip[idx], false)
-            // });
         });
     }
 
     // 現時点ではローカルに置いているが将来的にはクラウド管理がいい
     forDebug() {
-        this.musics.push(new Item('Protocol Omega', 'resources/musics/ADDrumnBass3/1.mp3'));
-        this.musics.push(new Item('Open your eyes you freak', 'resources/musics/ADDrumnBass3/2.mp3'));
-        this.musics.push(new Item('Black Future', 'resources/musics/ADDrumnBass3/3.mp3'));
-        this.musics.push(new Item('Formula', 'resources/musics/ADDrumnBass3/4.mp3'));
-        this.musics.push(new Item('Eruption', 'resources/musics/ADDrumnBass3/5.mp3'));
-        this.musics.push(new Item('Hacker', 'resources/musics/ADDrumnBass3/6.mp3'));
-        this.musics.push(new Item('4_3', 'resources/musics/ADDrumnBass3/7.mp3'));
-        this.musics.push(new Item('8', 'resources/musics/ADDrumnBass3/8.mp3'));
+        this.musics.push(new Item('Protocol Omega', 'musics/ADDrumnBass3'));
+        this.musics.push(new Item('Open your eyes you freak', 'musics/ADDrumnBass3'));
+        this.musics.push(new Item('Black Future', 'musics/ADDrumnBass3'));
+        this.musics.push(new Item('Formula', 'musics/ADDrumnBass3'));
+        this.musics.push(new Item('Eruption', 'musics/ADDrumnBass3'));
+        this.musics.push(new Item('Hacker', 'musics/ADDrumnBass3'));
+        this.musics.push(new Item('4_3', 'musics/ADDrumnBass3'));
+        this.musics.push(new Item('8', 'musics/ADDrumnBass3'));
     }
     // update (dt) {}
 }
