2f07baf98787d265d7f9ff8302c5d6863a518f62
shumurai <shu.murai3@gmail.com>
2019-09-02 21:59:46 +0900

    いろいろ問題見っけ

--- assets/Script/Menu/StartMenu.ts
+++ assets/Script/Menu/StartMenu.ts
@@ -13,7 +13,6 @@ export default class StartMenu extends cc.Component {
 
     itemPrefab: cc.Prefab = null;
     musics: string[][] = new Array();
-    clickEventHandler: cc.Component.EventHandler = new cc.Component.EventHandler();
     audioId: number = null;
     backgroundView: cc.Sprite = null;
     currentBackgroundView: string = null;
@@ -25,19 +24,6 @@ export default class StartMenu extends cc.Component {
     // LIFE-CYCLE CALLBACKS:
 
     onLoad() {
-        cc.loader.loadResDir('prefab', cc.Prefab, (err, item) => {
-            if (err) {
-                cc.error(err);
-                return;
-            }
-            this.itemPrefab = item[0];
-            this.dialogPrefab = item[1];
-        });
-    }
-
-    start() {
-        this.clickEventHandler.component = "StartMenu";
-        this.clickEventHandler.handler = "onClickMusicTitle";
         this.node.on(cc.Node.EventType.TOUCH_END, this.onTouchEnd, this);
         this.backgroundView = this.node.children[1].children[0].getComponent(cc.Sprite);
 
@@ -45,84 +31,100 @@ export default class StartMenu extends cc.Component {
 
         // TODO: Get Resources From Network
         this.forDebug();
-
-        let array = this.musics;
-        this.currentBackgroundView = array[0][2];
-        // 7以下の場合（あり得ない）は足りるように追加（後ほどあり得るかもしれないけど…）
-        // while (array.length < 7) {
-        //     let add = array[array.length - 2]
-        //     array.push(add);
-        // }
-
-        array.forEach((musicinfo) => {
-            let item = cc.instantiate(this.itemPrefab);
-            item.parent = this.content;
-            let itemClass: Item = item.getComponent(Item);
-            cc.loader.loadRes(musicinfo[1], cc.AudioClip, (err, clip: cc.AudioClip) => {
-                if (err) {
-                    cc.error(err);
-                    return;
-                }
-                itemClass.setParam(musicinfo[0], clip, musicinfo[2]);
-            });
-            // EventHandlerは1.0.0のAPIのため使わない(使えない？)
-            // node.on()に書き換える
-            this.clickEventHandler.target = item;
-            item.getComponent(cc.Button).clickEvents.push(this.clickEventHandler);
-            this.contentChild.push(item);
-        });
-
-        // なぜかItem以下がundefindになる
-        // let firstMusic = this.contentChild[0];
-        // firstMusic.children[1].setScale(1.2, 1.2);
-
-        // let firstMusicItem = this.contentChild[0].getComponent(Item);
-        // this.audioId = cc.audioEngine.playMusic(firstMusicItem.clip, false);
-        // this.bgmTitle = firstMusicItem.title;
-        // console.log(this.bgmTitle)
-        // cc.loader.loadRes(firstMusicItem.backgroundImage, cc.SpriteFrame, (err, res) => {
-        //     if (err) {
-        //         cc.error(err);
-        //         return;
-        //     }
-        //     this.backgroundView.spriteFrame = res;
-        // });
     }
 
-    onClickMusicTitle(event: cc.Event) {
+    start() {
+        let array = this.musics;
+        this.currentBackgroundView = array[0][2];
+
+        cc.loader.loadResDir('prefab', cc.Prefab, (err, item) => {
+            if (err) {
+                cc.error(err);
+                return;
+            }
+            this.dialogPrefab = item[0];
+            this.itemPrefab = item[1];
+            // 7以下の場合（あり得ない）は足りるように追加（後ほどあり得るかもしれないけど…）
+            // while (array.length < 7) {
+            //     let add = array[array.length - 2]
+            //     array.push(add);
+            // }
+            array.forEach((musicinfo) => {
+                let item = cc.instantiate(this.itemPrefab);
+                item.parent = this.content;
+                cc.loader.loadRes(musicinfo[1], cc.AudioClip, (err, clip: cc.AudioClip) => {
+                    if (err) {
+                        cc.error(err);
+                        return;
+                    }
+                    item.getComponent(Item).setParam(musicinfo[0], clip, musicinfo[2]);
+                });
+                item.on(cc.Node.EventType.TOUCH_START, this.onTouchStartMusicTitle, item);
+                item.on(cc.Node.EventType.TOUCH_END, this.onTouchEndMusicTitle, item);
+                this.contentChild.push(item);
+            });
+
+            // // なぜかItem以下がundefindになる
+            // let firstMusic = this.contentChild[0];
+            // firstMusic.children[1].setScale(1.2, 1.2);
+
+            // let firstMusicItem = this.contentChild[0].getComponent(Item);
+            // this.audioId = cc.audioEngine.playMusic(firstMusicItem.clip, false);
+            // this.bgmTitle = firstMusicItem.title;
+            // console.log(this.bgmTitle)
+            // cc.loader.loadRes(firstMusicItem.backgroundImage, cc.SpriteFrame, (err, res) => {
+            //     if (err) {
+            //         cc.error(err);
+            //         return;
+            //     }
+            //     this.backgroundView.spriteFrame = res;
+            // });
+        });
+    }
+
+    onTouchStartMusicTitle(event: cc.Event) {
+        let node: cc.Node = event.target;
+        node.setScale(1.2, 1.2);
+    }
+
+    onTouchEndMusicTitle(event: cc.Event) {
         let node: cc.Node = event.target;
         selectedMusic = [node.getComponent(Item).title, node.getComponent(Item).clip];
-        console.log(selectedMusic)
+        node.setScale(1, 1);
 
         // 難易度選択ダイアログを出す
-        // let dialog = cc.instantiate(this.dialogPrefab);
-        // dialog.getComponent(Dialog).showDialog(dialogType.difficulty);
+        let dialog = cc.instantiate(this.dialogPrefab);
+        dialog.getComponent(Dialog).showDialog(dialogType.difficulty);
     }
 
     // 曖昧な位置にいたとき近くの選択肢に移動する
     onTouchEnd() {
-        // contentの高さの絶対値から初期値分引いたもの
-        let absY = this.content.position.y < 0 ? -this.content.position.y - 74 : this.content.position.y - 74;
-        // absYをItemの高さ(148?)で割った余り
-        let distanceNear = absY % 148;
-        let moveTo: number = this.content.position.y;
+        // // contentの高さの絶対値から初期値分引いたもの
+        // let absY = this.content.position.y < 0 ? -this.content.position.y : this.content.position.y;
+        // // absYをItemの高さ(148?)で割った余り
+        // let distanceNear = absY % 148;
+        // let moveTo: number = this.content.position.y;
 
-        if (distanceNear < 37) { // 上にフォーカスする
-            moveTo -= (74 - distanceNear);
-        } else if (distanceNear < 111) { // フォーカスを選択していたものに戻す
-            moveTo += (74 - distanceNear);
-        } else { // 下にフォーカスする
-            moveTo += (222 - distanceNear);
-        }
+        // if (distanceNear < 37) { // 上にフォーカスする
+        //     moveTo -= (74 - distanceNear);
+        // } else if (distanceNear < 111) { // フォーカスを選択していたものに戻す
+        //     moveTo += (74 - distanceNear);
+        // } else { // 下にフォーカスする
+        //     moveTo += (222 - distanceNear);
+        // }
 
-        // アニメーション
-        let tween = new cc.Tween().target(this.content)
-            .to(0.5, { position: new cc.Vec2(0, moveTo) }, { progress: null, easing: null }).start();
-        this.onScrolled();
+        // // アニメーション
+        // let tween = new cc.Tween().target(this.content)
+        //     .to(0.5, { position: new cc.Vec2(0, moveTo) }, { progress: null, easing: null })
+        //     .start();
+        // this.onScrolled();
     }
 
     onScrolled() {
-        this.contentChild.forEach(item => {
+        this.contentChild.forEach((item, idx) => {
+            if (this.selectionArea.containsRect(item.getBoundingBoxToWorld())) {
+                cc.log(idx);
+            }
             if (this.selectionArea.containsRect(item.getBoundingBoxToWorld())) {
                 item.children[1].setScale(1.2, 1.2);
                 // 選択対象が一つ前に選択していた音楽のアルバムと違っているとき、背景画像を変える
@@ -143,7 +145,7 @@ export default class StartMenu extends cc.Component {
                     this.bgmTitle = item.getComponent(Item).title;
                 }
             } else {
-                item.children.forEach(child => child.setScale(1, 1));
+                item.children[1].setScale(1, 1);
             }
         });
     }
