cd40be3e0ca89594d8db742cd2497391d7500013
shumurai <47444124+shumurai@users.noreply.github.com>
2019-09-06 12:55:42 +0900

    ノーツと譜面クラスを作成

--- assets/Script/Util/Setting.ts
+++ assets/Script/Util/Setting.ts
@@ -1,16 +1,28 @@
 export default class Setting {
 
 }
+
 export enum dialogType {
     difficulty = 0,
     setting = 1,
     pause = 2,
 }
+
 export class SelectedMusic {
     public title: string = null;
     public path: string = null;
     public clip: cc.AudioClip = null;
     public difficulty: string = null;
+}
+
+export class Note {
+    time: number | number[]
+    position: number | number[]
+}
+
+export class Score {
+    score: {[key: string]: Note[]}
+    result: number
 }
 
 export var musicSelection: SelectedMusic = new SelectedMusic();
